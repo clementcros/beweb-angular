73a67dc0dc7c8c6c5c8f134c2e60637b38bb1ee5
CLEMENT CROS <clement.cros@sequoiasoft.com>
2019-08-11 14:44:21 +0200

    step-1 :
    Création de tous les composants
    mise ne place Html
    préparation des composants TS

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,16 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { NavComponent } from './nav/nav.component';
+import { MovieListComponent } from './movie-list/movie-list.component';
+import { MovieSaveComponent } from './movie-save/movie-save.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavComponent,
+    MovieListComponent,
+    MovieSaveComponent
   ],
   imports: [
     BrowserModule
