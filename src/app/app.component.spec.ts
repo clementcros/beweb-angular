d98bbf48dad35228ba7f804afa5cc40556a90e1d
CLEMENT CROS <clement.cros@sequoiasoft.com>
2019-08-11 14:25:32 +0200

    init project

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -16,16 +16,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'beweb'`, () => {
+  it(`should have as title 'beweb-movies'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.debugElement.componentInstance;
-    expect(app.title).toEqual('beweb');
+    expect(app.title).toEqual('beweb-movies');
   });
 
   it('should render title in a h1 tag', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.debugElement.nativeElement;
-    expect(compiled.querySelector('h1').textContent).toContain('Welcome to beweb!');
+    expect(compiled.querySelector('h1').textContent).toContain('Welcome to beweb-movies!');
   });
 });
