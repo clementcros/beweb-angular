20e8bb47077adae32379deb5ff2815f21ff7c042
CLEMENT CROS <clement.cros@sequoiasoft.com>
2019-08-11 15:53:07 +0200

    step-2 récupérer les films depuis l'api

--- src/app/movie-list/movie-list.component.ts
+++ src/app/movie-list/movie-list.component.ts
@@ -1,5 +1,4 @@
 import { Component, OnInit } from '@angular/core';
-import { MovieService} from '../service/movie.service';
 
 @Component({
   selector: 'app-movie-list',
@@ -7,16 +6,7 @@ import { MovieService} from '../service/movie.service';
   styleUrls: ['./movie-list.component.css']
 })
 export class MovieListComponent implements OnInit {
-  // Permet de pouvoir utiliser le service Movie
-  constructor(
-    private movies: MovieService,
-  ) {}
-
+  constructor() {}
   ngOnInit() {
   }
-// Vous devez écrire la fonction qui va vous permettre de demander au service de récupérer les film
-//  Pour cela il vous faut récupérer les infos dans le champ Input du Html
-//  et pouvoir ensuite les afficher en retour.
-// Good luck ;)
-
 }
